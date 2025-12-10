7cb62b33f2bb217e27b938cb138f18da2f36b1ec
Alpesh Jadhav <djalpesh786@gmail.com>
2025-12-10 18:24:16 +0530

    fifth commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from models import Product
 from database import session, engine
 from sqlalchemy.orm import Session
@@ -7,6 +8,14 @@ import database_models
 app = FastAPI()
 
 database_models.Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_db():
     db = session()
